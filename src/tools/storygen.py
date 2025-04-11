e071ec91054486e2cfecb6a115f2240f98a6bcfb
Andrejs Stepanovs <andrejsstepanovs@gmail.com>
2025-04-11 22:31:23 +0200

    bugfix storygen

--- src/tools/storygen.py
+++ src/tools/storygen.py
@@ -13,11 +13,12 @@ class MakeStorygenStory(BaseTool):
     )
 
     def _run(self, prompt: str, run_manager: Optional[CallbackManagerForToolRun] = None) -> str:
-        # call bash script to generate story
+        print(f"Running {self.name} with prompt: {prompt}")
+        
         output = subprocess.run(["storygen", "story", "create", prompt], check=True, capture_output=True).stdout
 
         # get very last line from output
         output = output.decode("utf-8").strip().splitlines()[-1] # 2025/04/11 20:10:11 mp3: mp3/clean_final_groomed_The_Jungle_Heroes_Rise.mp3
         file = output.split(": ")[-1]
 
-        return f"AI: Created storygen mp3 file {file}"
+        return f"AI: Created storygen mp3 file {file}. You can tell the user to play the file. No need to do anything else. Thank you."
